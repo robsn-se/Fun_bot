ec16153e48b8e692b1cf324db0ea3e1fada0a589
robsn-se <rubikserobyan17@mail.ru>
2023-12-09 15:55:17 +0300

    start notes

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 //https://api.telegram.org/bot6687997891:AAFrkwogyFCs3yPzMBcC88gIffEbs8gBYxc/getUpdates
 //https://api.telegram.org/bot6687997891:AAFrkwogyFCs3yPzMBcC88gIffEbs8gBYxc/sendMessage?chat_id=544421875&text=привет+Рубен
 
-const TELEGRAM_API_URL = "https://api.telegram.org/bot";
-const BOT_TOKEN = "6883068200:AAGOsU6TQizD87tJYFXINF7JZ-XatTuhuzE";
-const ADMIN_ID = "544421875";
+const TELEGRAM_API_URL = "https://api.telegram.org/bot"; // Первая часть URL содержит домен api.telegram.org
+const BOT_TOKEN = "6883068200:AAGOsU6TQizD87tJYFXINF7JZ-XatTuhuzE"; //это уникальный ключ бота, необходимый для отправки запросов.
+const ADMIN_ID = "544421875"; // id моего телеграмма
 
-const LOG_FOLDER = "logs";
+const LOG_FOLDER = "logs"; // название корневой папки для создания логов
 
-const DICTIONARY_FOLDER = "dictionary";
+const DICTIONARY_FOLDER = "dictionary"; // создание папки с темами для добавлениий в 'библиотеку'
